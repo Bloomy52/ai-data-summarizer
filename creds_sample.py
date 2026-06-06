fd7b413e849112372c674691aa8b9c625dbebfc1
Louie Bloomberg <basis-gyros8g@icloud.com>
2026-06-05 22:45:02 -0500

    Update creds_sample.py template for multiple APIs

--- creds_sample.py
+++ creds_sample.py
@@ -1,5 +1,4 @@
-# creds.py template
-# RENAME THIS FILE TO creds.py BEFORE USE
-
-# Gemini API Key
-GEMINI_API_KEY = "API_KEY_HERE"
+# RENAME THIS FILE TO creds.py BEFORE USE!
+GEMINI_API_KEY = ""
+OPENAI_API_KEY = ""
+CLAUDE_API_KEY = ""
