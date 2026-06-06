d859b83bdc62d74ab6ffc5c445afd8bac835b793
Louie Bloomberg <basis-gyros8g@icloud.com>
2026-06-06 22:15:55 +0000

    Refactor import statements and add google_tokenizer function for tokenization

--- summarizer.py
+++ summarizer.py
@@ -8,7 +8,25 @@ import sys
 import datetime
 import csv
 
+# Importing AI Libraries
+import openai
+import anthropic
+from google import genai
+from google.genai import types
+from google.genai import local_tokenizer
+
 # Function Definitions
 # TODO: Implement summarization functions here
+
+geminiClient = genai.Client(api_key='GEMINI_API_KEY')
+
+
+# Tokenization Functios
+# Google GenAI Tokenizer
+def google_tokenizer(text):
+    tokenizer = local_tokenizer.LocalTokenizer(model_name='gemini-3.5-flash')
+    result = tokenizer.count_tokens(text)
+    return result
+
 
 
