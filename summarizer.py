bb43220b33c2a04086524cf4227a720054cfb053
Louie Bloomberg <basis-gyros8g@icloud.com>
2026-06-14 22:51:33 +0000

    Added the openai tokenizer function

--- summarizer.py
+++ summarizer.py
@@ -9,24 +9,34 @@ import datetime
 import csv
 
 # Importing AI Libraries
-import openai
+from openai import OpenAI
 import anthropic
 from google import genai
 from google.genai import types
 from google.genai import local_tokenizer
 
+prompt = # TODO: Add prompt here
+
 # Function Definitions
 # TODO: Implement summarization functions here
-
-geminiClient = genai.Client(api_key='GEMINI_API_KEY')
 
 
 # Tokenization Functios
 # Google GenAI Tokenizer
 def google_tokenizer(text):
+    client = genai.Client(api_key=creds.GEMINI_API_KEY)
     tokenizer = local_tokenizer.LocalTokenizer(model_name='gemini-3.5-flash')
     result = tokenizer.count_tokens(text)
     return result
 
-
+# OpenAI Tokenizer
+def openai_tokenizer(prompt, text):
+    client = OpenAI(api_key=creds.OPENAI_API_KEY)
+    response = client.responses.input_tokens.count(
+    model="gpt-5.4-mini",
+    instructions=prompt,
+    input=text,
+)
+    return response.input_tokens
+    
 
