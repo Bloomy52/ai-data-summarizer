5ce5a408a044f961c91c06aba5aaedeb11722884
Louie Bloomberg <basis-gyros8g@icloud.com>
2026-06-15 22:29:56 +0000

    commented the client initializer in the google_tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -12,7 +12,7 @@ from google.genai import local_tokenizer
 # Tokenization Functios
 # Google GenAI Tokenizer
 def google_tokenizer(prompt, text):
-    client = genai.Client(api_key=creds.GEMINI_API_KEY)
+    #client = genai.Client(api_key=creds.GEMINI_API_KEY)
     tokenizer = local_tokenizer.LocalTokenizer(model_name='gemini-2.5-flash')
     result = tokenizer.count_tokens(prompt + text)
     return result.total_tokens
