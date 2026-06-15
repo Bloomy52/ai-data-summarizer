827aca9c98e1c4f00630f47a7fad450fe97b38b5
Louie Bloomberg <basis-gyros8g@icloud.com>
2026-06-15 03:18:54 +0000

    Added tokenization functions

--- main.py
+++ main.py
@@ -17,16 +17,26 @@ def check_tokens_gemini(text):
     # This function checks the number of tokens to make sure that they are within the Free Tier limits
     google_tokens = google_tokenizer(text)
     if creds.GEMINI_FREE_TIER == True and google_tokens > 250000:
-        print(f"Warning: Your input text has {google_tokens} tokens, which exceeds the free tier limit of 250,000 tokens for Gemini. Consider reducing the input size or upgrading your plan.")
+        print(f"Warning: Your input text has {google_tokens} tokens, which exceeds the free tier limit of 250,000 tokens per minute for Gemini. Consider reducing the input size or upgrading your plan.")
     elif creds.GEMINI_FREE_TIER == True:
         print(f"Your input text has {google_tokens} tokens, which is within the free tier limit for Gemini.")
     # TODO (maybe): Add cost functionality to estimate cost of input response
 
 
+def check_tokens_openai(prompt, text):
+    tokens = openai_tokenizer(prompt, text)
+    print(f"Your input text has {tokens} tokens.")
+
+
+def check_tokens_anthropic(prompt, text):
+    tokens = anthropic_tokenizer(prompt, text)
+    print(f"Your input text has {tokens} tokens.")
+
+
 # Main Function
 def main():
     # TODO: Implement main function logic
-    # Main Interactive Filename Loop:
+    # Main Interactive Loop:
     # Get input file
     while True:
         filepath = input("\nEnter the path to your CSV file: ").strip()
