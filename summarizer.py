9b915ae1330b1079b8f7608983e888677eff98ff
Louie Bloomberg <basis-gyros8g@icloud.com>
2026-06-17 16:45:39 +0000

    Created a separate writing outfile function and removed it from summarization function

--- summarizer.py
+++ summarizer.py
@@ -19,6 +19,34 @@ from google.genai import types
 # Function Definitions
 # TODO: Implement summarization functions here
 
+# Write File Function
+def write_outfile(output, filename, prompt_type, model_id):
+    # Create summaries directory if it doesn't exist
+    summaries_dir = f"./summaries/{prompt_type}"
+    os.makedirs(summaries_dir, exist_ok=True)
+    
+    now = dt.datetime.now()
+    date_time_string = now.strftime("%Y-%m-%d %H-%M-%S")
+    central_tz = ZoneInfo("America/Chicago")
+    date_written = dt.datetime.now(central_tz).strftime("%A, %B %d, %Y")
+    time_written = dt.datetime.now(central_tz).strftime("%I:%M %p %Z")
+    summary_local_path = f"{summaries_dir}/{date_time_string}.{model_id}.{filename}.txt"
+    with open(summary_local_path, "w") as outfile:
+        # Add Header Section to the output file
+        outfile.write("=" * 10 + "BEGIN HEADER" + "=" * 10 + "\n")
+        outfile.write("Date Written: " + date_written + "\n")
+        outfile.write("Time Written: " + time_written + "\n")
+        outfile.write("Model Used: " + model_id + "\n")
+        outfile.write("Prompt Type: " + prompt_type + "\n")
+        outfile.write("File Name: " + filename + "\n")
+        outfile.write("=" * 10 + "END HEADER" + "=" * 10 + "\n\n")
+        outfile.write("" + "=" * 10 + "BEGIN SUMMARY" + "=" * 10 + "\n\n")
+        
+        # Add main summary output to the output file
+        outfile.write(output)
+    
+    return None
+
 # Gemini Summarizer
 def gemini_summarizer(prompt, text, filename, prompt_type):
     MODEL_ID = "gemini-3.5-flash"
@@ -37,26 +65,5 @@ def gemini_summarizer(prompt, text, filename, prompt_type):
         print(f"Error generating summary: {e}")
         return None
     
-    # Create summaries directory if it doesn't exist
-    summaries_dir = f"./summaries/{prompt_type}"
-    os.makedirs(summaries_dir, exist_ok=True)
-    
-    now = dt.datetime.now()
-    date_time_string = now.strftime("%Y-%m-%d %H-%M-%S")
-    central_tz = ZoneInfo("America/Chicago")
-    date_written = dt.datetime.now(central_tz).strftime("%A, %B %d, %Y")
-    time_written = dt.datetime.now(central_tz).strftime("%I:%M %p %Z")
-    summary_local_path = f"{summaries_dir}/{MODEL_ID}.{filename}.{date_time_string}.txt"
-    with open(summary_local_path, "w") as outfile:
-        # Add Header Section to the output file
-        outfile.write("=" * 10 + "BEGIN HEADER" + "=" * 10 + "\n")
-        outfile.write("Date Written: " + date_written + "\n")
-        outfile.write("Time Written: " + time_written + "\n")
-        outfile.write("Model Used: " + MODEL_ID + "\n")
-        outfile.write("Prompt Type: " + prompt_type + "\n")
-        outfile.write("=" * 10 + "END HEADER" + "=" * 10 + "\n\n")
-        outfile.write("" + "=" * 10 + "BEGIN SUMMARY FOR " + filename + " " + "=" * 10 + "\n")
-        
-        # Add main summary output to the output file
-        outfile.write(output)
+    write_outfile(output, filename, prompt_type, MODEL_ID)
     return output
