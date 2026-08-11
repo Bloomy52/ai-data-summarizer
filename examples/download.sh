5174f58a7498f1be0c87e3707eef90f3c2a55773
Louie Bloomberg <basis-gyros8g@icloud.com>
2026-08-11 12:23:05 -0500

    created the bash script for downloading examples and added script header

---
+++ examples/download.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Example Downloading Script
+# download.sh
+# This script downloads up-to-date examples from original sources and saves them to the examples directory.
+# SPDX-License-Identifier: MIT
